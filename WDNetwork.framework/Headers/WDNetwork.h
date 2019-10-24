4a40018ba3d0c6fba973ebab8851b1378054f09d
Carlos Cáceres Watchman Door <ccaceres@watchman.es>
2019-10-24 14:17:53 +0200

    add wdnetwork framework

---
+++ WDNetwork.framework/Headers/WDNetwork.h
@@ -0,0 +1,19 @@
+//
+//  WDNetwork.h
+//  WDNetwork
+//
+//  Created by Carlos Cáceres González on 19/02/2019.
+//  Copyright © 2019 Watchman Door S.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WDNetwork.
+FOUNDATION_EXPORT double WDNetworkVersionNumber;
+
+//! Project version string for WDNetwork.
+FOUNDATION_EXPORT const unsigned char WDNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WDNetwork/PublicHeader.h>
+
+
